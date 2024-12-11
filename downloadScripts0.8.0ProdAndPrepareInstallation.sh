249aef18981199538194a5c89f45892c63875c05
Dimitrios Galanis <galanisd@gmail.com>
2024-12-11 17:22:04 +0200

    Update downloadScripts0.8.0ProdAndPrepareInstallation.sh

--- downloadScripts0.8.0ProdAndPrepareInstallation.sh
+++ downloadScripts0.8.0ProdAndPrepareInstallation.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+echo "git clone"
+git clone https://github.com/LanguageDataSpace/Deployment.git
+
+echo "Get into the directory"
+cd Deployment
+
+echo "list branches"
+git branch -a
+
+echo "checkout"
+git checkout -b 0.8.0 origin/0.8.0
+
+echo "pull"
+git pull
+
+echo "cd to docker-compose and create required directrories"
+cd docker-compose
+bash prepare.sh
