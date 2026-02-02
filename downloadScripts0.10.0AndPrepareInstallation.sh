adcf6150028305f07de69417f04a8eddd0872a00
Dimitrios Galanis <galanisd@gmail.com>
2026-02-02 19:41:40 +0200

    Update downloadScripts0.10.0AndPrepareInstallation.sh

--- downloadScripts0.10.0AndPrepareInstallation.sh
+++ downloadScripts0.10.0AndPrepareInstallation.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+VERS=0.10.0 
+
+echo "git clone"
+git clone https://github.com/LanguageDataSpace/Deployment.git Deployment$VERS
+
+echo "Get into the directory"
+cd Deployment$VERS
+
+echo "list branches"
+git branch -a
+
+echo "checkout"
+git checkout -b $VERS origin/$VERS
+
+echo "pull"
+git pull
+
+echo "cd to docker-compose and create required directrories"
+cd docker-compose
+bash prepare.sh
